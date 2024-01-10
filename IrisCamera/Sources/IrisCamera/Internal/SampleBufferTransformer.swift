a2484d9d8dedce060f9eaa80e0b5de1189940101
Jessica Santana <jeafsantana@hotmail.com>
2024-01-10 12:33:24 -0300

    create helper to apply color invert filter in pixel buffer

--- IrisCamera/Sources/IrisCamera/Internal/SampleBufferTransformer.swift
+++ IrisCamera/Sources/IrisCamera/Internal/SampleBufferTransformer.swift
@@ -3,19 +3,36 @@ import CoreImage
 import Foundation
 
 struct SampleBufferTransformer {
+    private let filterName = "CIColorInvert"
+    
     func transform(videoSampleBuffer: CMSampleBuffer) -> CMSampleBuffer {
         guard let pixelBuffer = CMSampleBufferGetImageBuffer(videoSampleBuffer) else {
             print("failed to get pixel buffer")
             fatalError()
         }
-
+        
         #warning("Implementation to invert colors in pixel buffer")
-
-
+        pixelBuffer.applyFilter(with: filterName)
+        
         guard let result = try? pixelBuffer.mapToSampleBuffer(timestamp: videoSampleBuffer.presentationTimeStamp) else {
+            print("failed to mapToSampleBuffer")
             fatalError()
         }
-
+        
         return result
+    }
+}
+
+extension CVImageBuffer {
+    func applyFilter(with name: String) {
+        let image = CIImage(cvImageBuffer: self)
+        
+        guard let filter = CIFilter(name: name, parameters: [kCIInputImageKey: image]),
+              let filteredImage = filter.outputImage else {
+                  print("failed to apply filter")
+                  fatalError()
+              }
+        
+        CIContext().render(filteredImage, to: self)
     }
 }
